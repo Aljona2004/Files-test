8fa41fb547b98159df152498cfb48c89060e469d
Opilane TTHK <opilane@tthk.ee>
2022-01-18 11:11:22 +0200

    first commit

---
+++ funfacts/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace funfacts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(RandomLineFromFile());
+            }
+        }
+        private static string RandomLineFromFile()
+        {
+            string filePath = @"C:\Users\opilane\Samples\funfacts.txt";
+
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, dataFromFile.Length);
+
+            return dataFromFile[randomIndex];
+        }
+    }
+}
---
+++ test files/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace test_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string newDirectory = "Samples";
+
+            CreateMyDirectory(newDirectory);
+        }
+        private static void CreateMyDirectory(string myDirectoryName)
+        {
+            string rootDirectory = @"C:\Users\opilane\samples";
+            string directoryFullPath = $@"{rootDirectory}\{myDirectoryName}";
+            if (Directory.Exists(directoryFullPath))
+            {
+                Console.WriteLine($"Directory {myDirectoryName} already exists in {rootDirectory}.");
+            }
+            else
+            {
+                Directory.CreateDirectory(directoryFullPath);
+                Console.WriteLine($"Directory {myDirectoryName} has been created.");
+            }
+        }
+        private static string[] ReadDataFromFile()
+        {
+            string directoryPath = @"C:\Users\opilane\Samples";
+            string fileName = "stars.txt; planets.txt";
+            string fullPath = $@"{directoryPath}\{fileName}";
+
+            string[] dataFromFile = File.ReadAllLines(fullPath);
+
+            return dataFromFile;
+        }
+        private static string GetLinesFromfileData(string userInput)
+        {
+            string[] dataFromFile = ReadDataFromFile();
+            string result = "";
+
+            foreach (string line in dataFromFile)
+            {
+                if (line.Contains(userInput))
+                {
+                    result = line;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        
+
+    }
+}
